8655b410f5ac720b61b656c23822fa39a8322a68
Benaouda Rayan Belguendouz <benaouda.belguendouz@eisti.fr>
2017-05-23 17:26:02 +0200

    Ajout Paccueil

--- src/Paccueil.java
+++ src/Paccueil.java
@@ -2,107 +2,115 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Paccueil extends JFrame 
+public class Paccueil extends JFrame
 {
-static Paccueil thePaccueil;
+   static Paccueil thePaccueil;
 
-JPanel pnPresentation;
-ButtonGroup rbgPresentation;
-JTextField tfBienvenue;
-JToggleButton tbtBoutonForm;
-JToggleButton tbtBoutonAdmin;
-JTextArea taArea0;
-/**
- */
-public static void main( String args[] ) 
-{
-   try 
+   JPanel pnPresentation;
+   ButtonGroup rbgPresentation;
+   JTextArea tfBienvenue;
+   JToggleButton tbtBoutonForm;
+   JToggleButton tbtBoutonAdmin;
+   JTextArea taArea0;
+   /**
+    */
+   public static void main( String args[] )
    {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      try
+      {
+         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      }
+      catch ( ClassNotFoundException e )
+      {
+      }
+      catch ( InstantiationException e )
+      {
+      }
+      catch ( IllegalAccessException e )
+      {
+      }
+      catch ( UnsupportedLookAndFeelException e )
+      {
+      }
+      thePaccueil = new Paccueil();
    }
-   catch ( ClassNotFoundException e ) 
+
+   /**
+    */
+   public Paccueil()
    {
+      super( "LES FORMUS DU TURFU" );
+
+      pnPresentation = new JPanel();
+      rbgPresentation = new ButtonGroup();
+      GridBagLayout gbPresentation = new GridBagLayout();
+      GridBagConstraints gbcPresentation = new GridBagConstraints();
+      pnPresentation.setLayout( gbPresentation );
+
+      tfBienvenue = new JTextArea("Bienvenue" );
+      gbcPresentation.gridx = 4;
+      gbcPresentation.gridy = 2;
+      gbcPresentation.gridwidth = 12;
+      gbcPresentation.gridheight = 3;
+      gbcPresentation.fill = GridBagConstraints.BOTH;
+      gbcPresentation.weightx = 1;
+      gbcPresentation.weighty = 0;
+      gbcPresentation.anchor = GridBagConstraints.NORTH;
+      gbPresentation.setConstraints( tfBienvenue, gbcPresentation );
+      pnPresentation.add( tfBienvenue );
+
+      tbtBoutonForm = new JToggleButton( "Répondre à un formulaire"  );
+      tbtBoutonForm.setSelected( true );
+      rbgPresentation.add( tbtBoutonForm );
+      gbcPresentation.gridx = 2;
+      gbcPresentation.gridy = 15;
+      gbcPresentation.gridwidth = 7;
+      gbcPresentation.gridheight = 3;
+      gbcPresentation.fill = GridBagConstraints.BOTH;
+      gbcPresentation.weightx = 1;
+      gbcPresentation.weighty = 0;
+      gbcPresentation.anchor = GridBagConstraints.NORTH;
+      gbPresentation.setConstraints( tbtBoutonForm, gbcPresentation );
+      pnPresentation.add( tbtBoutonForm );
+
+      tbtBoutonAdmin = new JToggleButton( "Accéder à la partie administrateur"  );
+      tbtBoutonAdmin.setSelected( true );
+      rbgPresentation.add( tbtBoutonAdmin );
+      gbcPresentation.gridx = 11;
+      gbcPresentation.gridy = 15;
+      gbcPresentation.gridwidth = 7;
+      gbcPresentation.gridheight = 3;
+      gbcPresentation.fill = GridBagConstraints.BOTH;
+      gbcPresentation.weightx = 1;
+      gbcPresentation.weighty = 0;
+      gbcPresentation.anchor = GridBagConstraints.NORTH;
+      gbPresentation.setConstraints( tbtBoutonAdmin, gbcPresentation );
+      pnPresentation.add( tbtBoutonAdmin );
+
+      JTextArea taArea0 = new JTextArea(
+              "Logiciel de traitement de formulaire.\n " +
+                      "Produit par moi  \n " +
+                      "Réalisé par moi \n " +
+                      "Pensé par moi \n"
+      );
+      taArea0.setFont(new Font("Serif", Font.ITALIC, 16));
+      taArea0.setLineWrap(true);
+      taArea0.setWrapStyleWord(true);
+      gbcPresentation.gridx = 1;
+      gbcPresentation.gridy = 6;
+      gbcPresentation.gridwidth = 18;
+      gbcPresentation.gridheight = 7;
+      gbcPresentation.fill = GridBagConstraints.BOTH;
+      gbcPresentation.weightx = 1;
+      gbcPresentation.weighty = 1;
+      gbcPresentation.anchor = GridBagConstraints.NORTH;
+      gbPresentation.setConstraints( taArea0, gbcPresentation );
+      pnPresentation.add( taArea0 );
+
+      setDefaultCloseOperation( EXIT_ON_CLOSE );
+
+      setContentPane( pnPresentation );
+      pack();
+      setVisible( true );
    }
-   catch ( InstantiationException e ) 
-   {
-   }
-   catch ( IllegalAccessException e ) 
-   {
-   }
-   catch ( UnsupportedLookAndFeelException e ) 
-   {
-   }
-   thePaccueil = new Paccueil();
-} 
-
-/**
- */
-public Paccueil() 
-{
-   super( "TITLE" );
-
-   pnPresentation = new JPanel();
-   rbgPresentation = new ButtonGroup();
-   GridBagLayout gbPresentation = new GridBagLayout();
-   GridBagConstraints gbcPresentation = new GridBagConstraints();
-   pnPresentation.setLayout( gbPresentation );
-
-   tfBienvenue = new JTextField( );
-   gbcPresentation.gridx = 4;
-   gbcPresentation.gridy = 2;
-   gbcPresentation.gridwidth = 12;
-   gbcPresentation.gridheight = 3;
-   gbcPresentation.fill = GridBagConstraints.BOTH;
-   gbcPresentation.weightx = 1;
-   gbcPresentation.weighty = 0;
-   gbcPresentation.anchor = GridBagConstraints.NORTH;
-   gbPresentation.setConstraints( tfBienvenue, gbcPresentation );
-   pnPresentation.add( tfBienvenue );
-
-   tbtBoutonForm = new JToggleButton( ""  );
-   tbtBoutonForm.setSelected( true );
-   rbgPresentation.add( tbtBoutonForm );
-   gbcPresentation.gridx = 2;
-   gbcPresentation.gridy = 15;
-   gbcPresentation.gridwidth = 7;
-   gbcPresentation.gridheight = 3;
-   gbcPresentation.fill = GridBagConstraints.BOTH;
-   gbcPresentation.weightx = 1;
-   gbcPresentation.weighty = 0;
-   gbcPresentation.anchor = GridBagConstraints.NORTH;
-   gbPresentation.setConstraints( tbtBoutonForm, gbcPresentation );
-   pnPresentation.add( tbtBoutonForm );
-
-   tbtBoutonAdmin = new JToggleButton( ""  );
-   tbtBoutonAdmin.setSelected( true );
-   rbgPresentation.add( tbtBoutonAdmin );
-   gbcPresentation.gridx = 11;
-   gbcPresentation.gridy = 15;
-   gbcPresentation.gridwidth = 7;
-   gbcPresentation.gridheight = 3;
-   gbcPresentation.fill = GridBagConstraints.BOTH;
-   gbcPresentation.weightx = 1;
-   gbcPresentation.weighty = 0;
-   gbcPresentation.anchor = GridBagConstraints.NORTH;
-   gbPresentation.setConstraints( tbtBoutonAdmin, gbcPresentation );
-   pnPresentation.add( tbtBoutonAdmin );
-
-   taArea0 = new JTextArea(2,10);
-   gbcPresentation.gridx = 1;
-   gbcPresentation.gridy = 6;
-   gbcPresentation.gridwidth = 18;
-   gbcPresentation.gridheight = 7;
-   gbcPresentation.fill = GridBagConstraints.BOTH;
-   gbcPresentation.weightx = 1;
-   gbcPresentation.weighty = 1;
-   gbcPresentation.anchor = GridBagConstraints.NORTH;
-   gbPresentation.setConstraints( taArea0, gbcPresentation );
-   pnPresentation.add( taArea0 );
-
-   setDefaultCloseOperation( EXIT_ON_CLOSE );
-
-   setContentPane( pnPresentation );
-   pack();
-   setVisible( true );
-} 
-} 
+}
